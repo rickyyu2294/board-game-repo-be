5a572418fbd32b98a165f8623901f13c207e4c16
Ricky Yu <rickyyu2294@gmail.com>
2024-02-25 12:17:35 -0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# boardGameRepo
+ Board Game Backend 
---
+++ src/test/kotlin/com/rickyyu/boardGameRepo/BoardGameRepoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rickyyu.boardGameRepo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardGameRepoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
